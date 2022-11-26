f22f3d1eb5135a59e98e88be7a7b0ac6f851cb4a
Yago <yago.lm@samsung.com>
2022-11-26 19:57:54 -0300

    update server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 import "dotenv/config"
+// import * as dotenv from "dotenv" // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
+
 import app from "./app"
+// require('dotenv').config()
+// const dotenv = require('dotenv')
 
 app.listen(process.env.PORT)
